ad780134ba655ef2da8c638d298d6920fa142f3a
maciej <maciejsto@gmail.com>
2013-09-04 14:09:13 +0200

    observer class modification

--- Observer/tests/ObserverTest.php
+++ Observer/tests/ObserverTest.php
@@ -1,1 +1,12 @@
 <?php
+
+class ObserverTest extends PHPUnit_Framework_TestCase
+{
+    //fixture
+    private $_observer;
+    
+    public function setUp()
+    {
+        $this->_observer = new Observer();
+    }
+}
